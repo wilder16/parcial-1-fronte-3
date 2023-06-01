3d70e0f36a1528bdb52e02c85f11b4588dd86bcd
Wilder Bernal <wjblah@gmail.com>
2023-05-31 20:56:31 -0500

    subiendo parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
+import Card from './Card';
+import { useState } from 'react'
+import './App.css'
+
+
 function App() {
-  //Aqui deberias agregar los estados y los handlers para los inputs
+  const [nombre, setNombre] = useState("")
+  const [errorInput, setErrorInput] = useState("");
+  const [color, setColor] = useState("")
+  const [mostrarCard, setMostrarCard] = useState(false);
+  const mensajeErrro = "Por favor chequea que la información sea correcta"
+
+
+  const handleSubtmit = (e) => {
+    e.preventDefault()
+
+    if(nombre.length < 3 || nombre.startsWith(' ')){
+      setErrorInput(mensajeErrro)
+      return
+    }else if(color.length < 6){
+      setErrorInput(mensajeErrro)
+      return
+    }else{
+      setMostrarCard(true)
+    }
+
+  }
+
+
 
   return (
-    <div className="App">
+    <div className="App" onSubmit={handleSubtmit}>
       <h1>Elige un color</h1>
-      <form>{/* aqui deberias escribir tu codigo*/}</form>
+      <form>
+        <input 
+        type="text"
+        placeholder="Ingrese tu nombre"
+        value={nombre}
+        onChange={e => setNombre(e.target.value)}
+        />
+        <input 
+        type="number" 
+        placeholder="Ingrese tu color favorito (formato HEX)"
+        value={color}
+        onChange={e => setColor(e.target.value)}
+        />
+        <button type="submit">ENVIAR</button>
+
+        {errorInput && <div className="error">{errorInput}</div>}
+      </form>
+      {mostrarCard && (
+          <Card nombre={nombre} color={color} />
+        )}
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,17 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
+function Card({nombre, color}) {
+
+  const buttonStyle = {
+    backgroundColor:color
+  };
+
   return (
     <div>
-      <h2>Esto es un componente</h2>
+      <h2>Hola {" "+nombre}</h2>
+      <h3>Sabenos que tu color favorito es:</h3>
+      <button style={buttonStyle} >#{color}</button>
     </div>
   );
 }
